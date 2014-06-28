bd83eb666240dcfca3d7ed528a00b2a0c72e9277
Victor C. Martins <dxhj@hotmail.com.br>
2014-06-28 19:47:10 -0300

    Update pyide.py

--- pyide.py
+++ pyide.py
@@ -7,21 +7,22 @@ $ wget https://github.com/4rsh/
 
 """
 
-# Naming the file.
-name = raw_input("Type a name for your file.\n")
-if len(name) < 4:
-	print "Try again.\n"
-else:
-	file = open(name)
-
-	# Writing.
-	coding = True
-	os.system("clear")
-	print "[Code: %s]" % name
-	print "\n\n"
-	while coding:
-		code = raw_input("> ")
-		file.write(code + "\n")
-
-	print "\n\n\nSaved >> %s" % name
-	file.close()
+while True:
+	name = raw_input("Type a name for your file.\n")
+	if len(name) < 4:
+		print "Try again.\n"
+	else:
+		file = open(name)
+		# Writing.
+		coding = True
+		os.system("clear")
+		print "[Code: %s]" % name
+		print "\n\n"
+		while coding:
+			code = raw_input("> ")
+			if code == "exit()":
+				coding = False
+			file.write(code + "\n")
+	
+		print "\n\n\nSaved >> %s" % name
+		file.close()
