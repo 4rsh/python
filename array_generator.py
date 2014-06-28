ac98a395cf9639fd2e88ae8bd4e31ea5624ef927
Arsh Leak <anonks@live.com>
2014-06-27 21:27:10 -0300

    Array Generator - Store your items automatically.

--- array_generator.py
+++ array_generator.py
@@ -4,10 +4,23 @@
 """
 Array Generator. — Developed by Arsh Leak.
 $ wget https://github.com/4rsh/
+
+HOW WORKS:
+$ python array_generator.py
+	Enter the name of your array.
+	>       name
+
+	Enter your strings.
+	>       Archiving Items Automatically
+
+  Result:
+	name Array:
+	name    =       ['Archiving', 'Items', 'Automatically']
+
 """
 
-name	=	raw_input("Enter the name of your array.\n>	")
-items	=	raw_input("Enter your strings.\n>	")
+name	=	raw_input("\nEnter the name of your array.\n>	")
+items	=	raw_input("\nEnter your strings.\n>	")
 array	=	items.split()
-print "\n\n%s Array:\n" % name
+print "\n\n%s Array:" % name
 print name + "	=	%s" % array
