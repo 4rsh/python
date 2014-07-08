de9aa55ce79e146ac318fca2da0a6a4ad91de7ee
Arsh Leak <anonks@live.com>
2014-07-08 18:18:09 -0300

    PyvãoBueno

---
+++ pyvaobueno.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+
+"""
+PyvãoBueno - Narração do Jogo do Brasil x Alemanha.
+
+Developed by Arsh Leak.
+$ wget https://github.com/4rsh/
+"""
+
+
+gols = 0
+while True:
+	print "Brasil	0	x	%s	Alemanha" % (gols)
+	gols += 1
