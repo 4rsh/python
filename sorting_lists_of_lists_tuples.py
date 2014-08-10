530696df2f1dbe4c347d15377d971db660cdcd41
Arsh Leak <arshleak@hotmail.com>
2014-08-10 11:12:14 -0300

    Delete sorting_lists_of_lists_tuples.py

--- sorting_lists_of_lists_tuples.py
+++
@@ -1,9 +0,0 @@
->>> data = [("Buggati Veyron", 100), ("Lamborghini Veneno", 96)]
->>> data.sort(key=lambda tup: tup[1]) # sorts by elements at index 1
->>> data
-[('Lamborghini Veneno', 96), ('Buggati Veyron', 100)]
-
->>> from operator import itemgetter
->>> data = [("gold", 100), ("silver", 50), ("bronze", 10)]
->>> sorted(data, key=itemgetter(1))
-[('bronze', 10), ('silver', 50), ('gold', 100)]
